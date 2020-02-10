50de92490fcd33bd91bfeeb3e145c39cba357036
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-09 19:06:08 -0600

    mongo

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+var router = express.Router();
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function (error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
+
+app.listen(PORT, function () {
+    console.log("Listening on port:" + PORT);
+});
+
