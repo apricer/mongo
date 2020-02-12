4314a86ccf00e36c0665a51bb80f146431783780
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-11 19:57:37 -0600

    mongo

--- config/routes.js
+++ config/routes.js
@@ -44,4 +44,30 @@ module.exports = function (router) {
             res.json(data);
         });
     });
+    router.patch("/api/headlines", function (req, res) {
+        headlinesController.update(req.body, function (err, data) {
+            res.json(data);
+        });
+    });
+    router.get("/api/notes/:headline_id?", function (req, res) {
+        var query = {};
+        if (req.params.headline_id) {
+            query._id = req.params.headline_id;
+        }
+        notesController.get(query, function (err, data) {
+            res.json(data);
+        });
+    });
+    router.delete("/api/notes/:id", function (req, res) {
+        var query = {};
+        query._id = req.params.id;
+        notesController.delete(query, function (err, data) {
+            res.json(data);
+        });
+    });
+    router.post("/api/notes", function (req, res) {
+        notesController.save(req.body, function (data) {
+            res.json(data);
+        });
+    });
 }
