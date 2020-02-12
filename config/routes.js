851442cff8bf6c4552aa860dd62ea61c0db6b3de
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-11 19:40:10 -0600

    mongo

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,9 @@
+var scrape = require("../scripts/scrape");
+
+var headlinesController = require("../controllers/headlines");
+
+var notesController = require("../controllers/notes");
+
 module.exports = function (router) {
     router.get("/", function (req, res) {
         res.render("home");
@@ -5,5 +11,37 @@ module.exports = function (router) {
 
     router.get("/saved", function (req, res) {
         res.render("saved");
+    });
+
+    router.get("/api/fetch", function (req, res) {
+        headlinesController.fetch(function (err, docs) {
+            if (!docs || docs.insertedCount === 0) {
+                res.json({
+                    message: "No new articles today. Check back tomorrow!"
+                });
+            }
+            else {
+                res.json({
+                    message: "Added " + docs.insertedCount + " new articles!"
+                });
+            }
+        });
+    });
+    router.get("/api/headlines", function (req, res) {
+        var query = {};
+        if (req.query.saved) {
+            query = req.query;
+        }
+
+        headlinesController.get(query, function (data) {
+            res.json(data);
+        });
+    });
+    router.delete("/api/headlines/.id", function (req, res) {
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function (err, data) {
+            res.json(data);
+        });
     });
 }
---
+++ controllers/notes.js
@@ -0,0 +1,33 @@
+var Note = require("../models/Note");
+
+var makeDate = require("../scripts/date");
+
+module.exports = {
+    get: function (data, cb) {
+        Note.find({
+            _headlineId: data._id
+        }, cb);
+    },
+    save: function (data, cb) {
+        var newNote = {
+            _headlineId: data._id,
+            date: makeDate(),
+            noteText: data.noteText
+        };
+
+        Note.create(newNote, function (err, doc) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(doc);
+                cb(doc);
+            }
+        });
+    },
+    delete: function (data, cb) {
+        Note: remove({
+            _id: data._id
+        }, cb);
+    }
+};
